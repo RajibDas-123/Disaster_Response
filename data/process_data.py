9c75ef67038fc834ee797d4f65e41a12278bce6d
Rajib <rajivlochand613@gmail.com>
2020-06-21 15:00:32 +0530

    Added template

---
+++ data/process_data.py
@@ -0,0 +1,79 @@
+import sys
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sqlalchemy import create_engine
+
+
+def load_data(messages_filepath, categories_filepath):
+    
+    messages = pd.read_csv("messages.csv")
+    categories = pd.read_csv("categories.csv")
+    df = pd.merge(messages, categories, on="id")
+    
+    categories = categories.categories.str.split(";", expand=True)
+    row = categories.loc[0]
+    
+    category_colnames = row.apply(lambda cat: cat[:-2])
+    
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda val: val[-1])
+
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+        
+    df = df.drop('categories', axis=1) 
+    
+    df = pd.concat([df, categories], axis=1)
+
+    return df
+
+
+def clean_data(df):
+    
+    df.iloc[:, 4:] = df.iloc[:,4:].applymap(lambda x: 1 if x > 1 else x)
+    
+    df = df.drop_duplicates(keep='first')
+    
+    df = df.dropna(subset=category_colnames, how='all')
+    
+    return df
+
+
+def save_data(df, database_filename):
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('disaster_data', engine, index=False)
+      
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,93 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql("SELECT * from disaster_data", engine)
+    X = df.message.values
+    y = df.iloc[:,4:].values
+    categories = df.iloc[:,4:].columns.values
+    return X, y, categories
+
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    stemmer = PorterStemmer()
+    tokens = [lemmatizer.lemmatize(token) for token in tokens if token not in stopwords.words("english")]
+    tokens = [stemmer.stem(token) for token in tokens]
+    return tokens
+        
+
+
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', RandomForestClassifier())
+    ])
+    
+    parameters = {
+        'tfidf__use_idf':[True, False],
+        'clf__criterion':["gini", "entropy"]
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    
+    return cv
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    y_pred = model.predict(X_test)
+    
+
+
+def save_model(model, model_filepath):      
+
+    with open(model_filepath, 'wb') as file:  
+        pickle.dump(model, file)
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
